31d46291cd621a217311c66ad989d2ad454029e6
AjitKul45 <apkulkarni243@gmail.com>
2023-03-21 17:18:57 +0530

    reactive form

--- fr/src/app/dashboard/components/Forms/add-vendor/add-vendor.component.ts
+++ fr/src/app/dashboard/components/Forms/add-vendor/add-vendor.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { ApplicationService } from 'src/app/services/application.service';
 import { IVendor } from 'src/app/dashboard/components/Forms/Models/ivendor';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-add-vendor',
@@ -9,28 +11,51 @@ import { IVendor } from 'src/app/dashboard/components/Forms/Models/ivendor';
   styleUrls: ['./add-vendor.component.css'],
 })
 export class AddVendorComponent {
+  contactNo: any;
+
   constructor(
-    private dashboardService: ApplicationService,
+    private applicationService: ApplicationService,
+
     private router: Router
   ) {}
 
-  vendor: IVendor = {
-    id: 0,
-    name: '',
-    contactNo: '',
-    address: '',
-    registrationDate: '',
-    terminationDate: '',
-  };
+  vendorForm = new FormGroup({
+    id: new FormControl('0'),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('[a-zA-Z]+([s][a-zA-Z]+)*'),
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    contactNo: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      Validators.minLength(10),
+    ]),
+    registrationDate: new FormControl(
+      formatDate(Date.now(), 'yyyy-MM-dd', 'en_US').toString()
+    ),
+    terminationDate: new FormControl('', [Validators.required]),
+  });
 
   submitVendorDetails() {
-    this.dashboardService.addVendor(this.vendor).subscribe(
-      (res) => {
-        alert('Vendor Added !');
-        // this.vendor = res;
-        this.router.navigate(['dashboard']);
-      },
-      (err) => {}
-    );
+    if (this.vendorForm.valid) {
+      this.applicationService
+        .addVendor(this.vendorForm.value)
+        .subscribe((res) => {
+          console.log(res);
+          this.applicationService.Show('Vendor added successfully');
+        });
+    }
+  }
+
+  get f() {
+    return this.vendorForm.controls;
+  }
+  notify() {
+    this.applicationService.Show('Notified success');
   }
 }
